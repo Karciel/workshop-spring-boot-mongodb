55d50c5527b6a630d510d614162afdb807286828
Gabriel V. Silva <karciel@outlook.com>
2024-01-23 11:28:22 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/gabrielvsilva/workshopmongo/domain/Post.java
+++ src/main/java/com/gabrielvsilva/workshopmongo/domain/Post.java
@@ -1,25 +1,31 @@
 package com.gabrielvsilva.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.gabrielvsilva.workshopmongo.dto.AuthorDTO;
+import com.gabrielvsilva.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
 	
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	public Post() {
-		
 	}
 
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
@@ -62,18 +68,29 @@ public class Post implements Serializable {
 	public void setBody(String body) {
 		this.body = body;
 	}
-	
+
 	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthorDTO(AuthorDTO author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -85,8 +102,11 @@ public class Post implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Post other = (Post) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
 }
